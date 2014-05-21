fc51aa423654bb833cadc768f13f5f5b7ec38532
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-05-21 23:57:13 +0200

    fix: server dir

--- test.sh
+++ test.sh
@@ -2,5 +2,6 @@
 pwd
 HOME=`pwd`
 cd ..
-openerp/openerp-server --stop-after-init --addons-path=addons,web/addons,$HOME -i connector_file -d travis_ci_test
-openerp/openerp-server --stop-after-init --addons-path=addons,web/addons,$HOME -i connector_file -d travis_ci_test --test-enable --log-level=test
+ls
+server/openerp-server --stop-after-init --addons-path=addons,web/addons,$HOME -i connector_file -d travis_ci_test
+server/openerp-server --stop-after-init --addons-path=addons,web/addons,$HOME -i connector_file -d travis_ci_test --test-enable --log-level=test
