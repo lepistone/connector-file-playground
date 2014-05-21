d342515e4ec5e430d149c99817efe1c66e06ef5f
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-05-21 20:09:55 +0200

    fix: clone, not checkout

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 set -e
 
 cd ..
-git checkout --depth 1 -b ocb-7.0 git://github.com/akretion/openerp-server server
-git checkout --depth 1 -b ocb-7.0 git://github.com/akretion/openerp-addons addons
-git checkout --depth 1 -b ocb-7.0 git://github.com/akretion/openerp-web web
+git clone --depth 1 -b ocb-7.0 git://github.com/akretion/openerp-server server
+git clone --depth 1 -b ocb-7.0 git://github.com/akretion/openerp-addons addons
+git clone --depth 1 -b ocb-7.0 git://github.com/akretion/openerp-web web
 
 cd server
 python setup.py --quiet install
