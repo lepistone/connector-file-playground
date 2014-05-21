f85ab329e68c00e2e43a7b70491f23ba4d77c31f
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-05-21 18:43:02 +0200

    add initial travis config

---
+++ install.sh
@@ -0,0 +1,13 @@
+set -e
+
+wget http://nightly.openerp.com/7.0/nightly/src/openerp-7.0-latest.tar.gz
+
+mkdir tmp
+tar -xf openerp-7.0-latest.tar.gz -C tmp
+mv tmp/* openerp
+rmdir tmp
+
+pwd
+ls
+cd openerp
+python setup.py install
