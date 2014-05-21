b22e3969292fd46ecd342fa22ce28a93a198b6e3
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-05-21 19:43:58 +0200

    ci: actually run the tests

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "YAY!"
-openerp/openerp-server --stop-after-init --addons-path=. -i connector-file -d travis_ci_test
+openerp/openerp-server --stop-after-init --addons-path=. -i connector_file -d travis_ci_test
+openerp/openerp-server --stop-after-init --addons-path=. -i connector_file -d travis_ci_test --test-enable --log-level=test
