750eb5203fcfaab46e0aad06b02504eb9524571b
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-05-22 00:51:01 +0200

    fix quotes

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 ADDONS_PATH=addons,web/addons,openerp-connector,`pwd`
 MODULES_UNDER_TEST=connector_file
 DBNAME=travis_ci_test
-OPTIONS=--addons-path=$ADDONS_PATH -i $MODULES_UNDER_TEST -d $DBNAME --stop-after-init
+OPTIONS="--addons-path=$ADDONS_PATH -i $MODULES_UNDER_TEST -d $DBNAME --stop-after-init"
 cd ..
 server/openerp-server $OPTIONS
 server/openerp-server $OPTIONS --test-enable --log-level=test
